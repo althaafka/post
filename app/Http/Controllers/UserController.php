d7014cd17cd20ed6e3a9a4852580561188f14994
althaafka <althaafka004@gmail.com>
2024-05-12 17:09:30 +0700

    feat: register, login, logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request){
         $user_input = $request->validate([
-            'name' => 'required|min:3|max:10',
-            'email' => 'required|email',
+            'name' => ['required','min:3','max:10', Rule::unique('users', 'name')],
+            'email' => 'required|email|unique:users',
             'password' => 'required|min:6'
         ]);
-        return 'thank you for registering';
+
+        $user_input['password'] = bcrypt($user_input['password']);
+        $user = User::create($user_input);
+        auth()->login($user);
+
+        return redirect('/');
+    }
+
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if(auth()->attempt(['name' => $credentials['loginname'], 'password' => $credentials['loginpassword']])){
+            $request->session()->regenerate();
+        }
+
+        return redirect('/');
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect('/');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,34 @@
     <title>Document</title>
 </head>
 <body>
-    <div style="border: 3px solid black">
-        <h2>Register</h2>
-        <form action="/register" method="POST">
+
+    @auth
+        <p>Congrats u are login</p>
+        <form action="/logout" method="POST">
             @csrf
-            <input type="text" name="name" placeholder="Name"><br><br>
-            <input type="email" name="email" placeholder="Email"><br><br>
-            <input type="password" name="password" placeholder="Password"><br><br>
-            <button>Register</button>
+            <button>Logout</button>
         </form>
-    </div>
+    @else
+        <div style="border: 3px solid black">
+            <h2>Register</h2>
+            <form action="/register" method="POST">
+                @csrf
+                <input type="text" name="name" placeholder="Name"><br><br>
+                <input type="email" name="email" placeholder="Email"><br><br>
+                <input type="password" name="password" placeholder="Password"><br><br>
+                <button>Register</button>
+            </form>
+        </div>
+        <div style="border: 3px solid black">
+            <h2>Login</h2>
+            <form action="/login" method="POST">
+                @csrf
+                <input type="text" name="loginname" placeholder="Name"><br><br>
+                <input type="password" name="loginpassword" placeholder="Password"><br><br>
+                <button>Login</button>
+            </form>
+        </div>
+    @endauth
+
 </body>
 </html>
